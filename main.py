46a6891bf38ff76cc2c0a81800efc856bd67c1ff
Anthony Montes <anthony_montes4@my.vcccd.edu>
2024-06-27 16:26:58 -0700

    Assignment 4-4

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
 
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    for i in range (5):
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    minval = numbers[0]
+    maxval = numbers[0]
+
+    for num in numbers[1:]:
+        if num < minval:
+            minval = num
+        if num > maxval:
+            maxval = num
 
     print(*numbers)
     print(maxval, minval)
